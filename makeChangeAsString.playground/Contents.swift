ec9de70d740667e3162845b3f7642e490c4fca4e
Jarren Campos <jarrenmcampos@gmail.com>
2020-06-01 21:01:43 -0700

    Few bugs, but logic about there

--- makeChangeAsString.playground/Contents.swift
+++ makeChangeAsString.playground/Contents.swift
@@ -1,8 +1,34 @@
 import UIKit
 
+func makeChangeAsString(fromAmount: Double, withCost: Double) -> String {
+    var amountHolder: Double
+    let amountHolder1: Double = -1.00
+    
+    amountHolder = fromAmount - withCost
+    
+    if String(amountHolder).contains("-"){
+        print("You didn't pay enough! You still owe $\(String(format: "%.2f",amountHolder * amountHolder1)). \(changeCounter(amountHolder: (amountHolder * amountHolder1)))")
+        return "Negative"
+    } else {
+        print("Your change is $\(String(format: "%.2f", amountHolder)). \(changeCounter(amountHolder: amountHolder))")
+    return "Positive"
+    }
+}
 
-
-
+func changeCounter(amountHolder: Double) -> String {
+    var holder: Double = 0
+    
+    let quarterValue: Double = 0.25
+    
+    let dollars = Int(amountHolder)
+    let quarters = Int((amountHolder - Double(dollars)) / quarterValue)
+    holder = (amountHolder - Double(dollars)) - Double(quarters) * quarterValue
+    let dimes = Int(holder * 10)
+    let nickels = 0
+    let pennies = Int(((holder * 10) - Double(dimes)) * 10)
+    
+    return "That is \(dollars) dollars, \(quarters) quarters, \(dimes) dimes, \(nickels) nickels, \(pennies) pennies."
+}
 
 
 
@@ -18,11 +44,11 @@ import UIKit
 
 
 // Test Cases
-//makeChangeAsString(fromAmount: 10.00, withCost: 2.38) // returns "Your change is $7.62. That is 7 dollars, 2 quarters, 1 dime, 0 nickels and 2 pennies."
-//makeChangeAsString(fromAmount: 5.00, withCost: 2.15)  // returns "Your change is $2.85. That is 2 dollars, 3 quarters, 1 dime, 0 nickels and 0 pennies."
-//makeChangeAsString(fromAmount: 1.00, withCost: 2.38)  // returns "You didn't pay enough! You still owe $1.38. That is 1 dollar, 1 quarter, 1 dime, 0 nickels and 3 pennies."
-//makeChangeAsString(fromAmount: 10.00, withCost: 8.00) // returns "Your change is $2.0. That is 2 dollars, 0 quarters, 0 dimes, 0 nickels and 0 pennies."
-//makeChangeAsString(fromAmount: 1.00, withCost: 0.50)  // returns "Your change is $0.5. That is 0 dollars, 2 quarters, 0 dimes, 0 nickels and 0 pennies."
-//makeChangeAsString(fromAmount: 1.00, withCost: 0.80)  // returns "Your change is $0.2. That is 0 dollars, 0 quarters, 2 dimes, 0 nickels and 0 pennies."
-//makeChangeAsString(fromAmount: 1.00, withCost: 0.95)  // returns "Your change is $0.05. That is 0 dollars, 0 quarters, 0 dimes, 1 nickel and 0 pennies."
-//makeChangeAsString(fromAmount: 1.00, withCost: 0.96)  // returns "Your change is $0.04. That is 0 dollars, 0 quarters, 0 dimes, 0 nickels and 4 pennies."
+makeChangeAsString(fromAmount: 10.00, withCost: 2.38) // returns "Your change is $7.62. That is 7 dollars, 2 quarters, 1 dime, 0 nickels and 2 pennies."
+makeChangeAsString(fromAmount: 5.00, withCost: 2.15)  // returns "Your change is $2.85. That is 2 dollars, 3 quarters, 1 dime, 0 nickels and 0 pennies."
+makeChangeAsString(fromAmount: 1.00, withCost: 2.38)  // returns "You didn't pay enough! You still owe $1.38. That is 1 dollar, 1 quarter, 1 dime, 0 nickels and 3 pennies."
+makeChangeAsString(fromAmount: 10.00, withCost: 8.00) // returns "Your change is $2.0. That is 2 dollars, 0 quarters, 0 dimes, 0 nickels and 0 pennies."
+makeChangeAsString(fromAmount: 1.00, withCost: 0.50)  // returns "Your change is $0.5. That is 0 dollars, 2 quarters, 0 dimes, 0 nickels and 0 pennies."
+makeChangeAsString(fromAmount: 1.00, withCost: 0.80)  // returns "Your change is $0.2. That is 0 dollars, 0 quarters, 2 dimes, 0 nickels and 0 pennies."
+makeChangeAsString(fromAmount: 1.00, withCost: 0.95)  // returns "Your change is $0.05. That is 0 dollars, 0 quarters, 0 dimes, 1 nickel and 0 pennies."
+makeChangeAsString(fromAmount: 1.00, withCost: 0.96)  // returns "Your change is $0.04. That is 0 dollars, 0 quarters, 0 dimes, 0 nickels and 4 pennies."
